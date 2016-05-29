7e50749a8b27c5101e760af3453d946216f94d64
learrsan <gaslas@hotmail.com>
2016-05-29 21:37:25 +0200

    29/05/2016
    21:37

---
+++ js/script.js
@@ -0,0 +1,127 @@
+﻿var x;
+x=$(document);
+x.ready(inicializar);
+			
+	function inicializar ()	{
+		var x;
+		x=$("#cuadroS1");
+		x.mouseover(mostrarTextoS1);
+		
+		var x;
+		x=$("#servicio1");
+		x.mouseout(ocultarTextoS1);
+		
+		var x;
+		x=$("#cuadroS2");
+		x.mouseover(mostrarTextoS2);
+			
+		var x;
+		x=$("#servicio2");
+		x.mouseout(ocultarTextoS2);	
+
+		var x;
+		x=$("#cuadroS3");
+		x.mouseover(mostrarTextoS3);
+			
+		var x;
+		x=$("#servicio3");
+		x.mouseout(ocultarTextoS3);	
+		
+		var x;
+		x=$("#cuadroS4");
+		x.mouseover(mostrarTextoS4);
+			
+		var x;
+		x=$("#servicio4");
+		x.mouseout(ocultarTextoS4);	
+		
+		var x;
+		x=$("#cuadroS5");
+		x.mouseover(mostrarTextoS5);
+			
+		var x;
+		x=$("#servicio5");
+		x.mouseout(ocultarTextoS5);	
+		
+		var x;
+		x=$("#cuadroS6");
+		x.mouseover(mostrarTextoS6);
+			
+		var x;
+		x=$("#servicio6");
+		x.mouseout(ocultarTextoS6);	
+	}
+			
+			function mostrarTextoS1(){
+				var x;
+				x=$("#servicio1");
+				x.show("blind",  300);
+			}
+			
+			function ocultarTextoS1(){
+				var x;
+				x=$("#servicio1");
+				x.hide("blind",  300);
+			}
+				
+			function mostrarTextoS2(){
+				var x;
+				x=$("#servicio2");
+				x.show("blind",  300);
+			}
+			
+			function ocultarTextoS2(){
+				var x;
+				x=$("#servicio2");
+				x.hide("blind",  300);
+			}
+			
+			function mostrarTextoS3(){
+				var x;
+				x=$("#servicio3");
+				x.show("blind",  300);
+			}
+			
+			function ocultarTextoS3(){
+				var x;
+				x=$("#servicio3");
+				x.hide("blind",  300);
+			}
+			
+			function mostrarTextoS4(){
+				var x;
+				x=$("#servicio4");
+				x.show("blind",  300);
+			}
+			
+			function ocultarTextoS4(){
+				var x;
+				x=$("#servicio4");
+				x.hide("blind",  300);
+			}
+			
+			function mostrarTextoS5(){
+				var x;
+				x=$("#servicio5");
+				x.show("blind",  300);
+			}
+			
+			function ocultarTextoS5(){
+				var x;
+				x=$("#servicio5");
+				x.hide("blind",  300);
+			}
+			
+			function mostrarTextoS6(){
+				var x;
+				x=$("#servicio6");
+				x.show("blind",  300);
+			}
+			
+			function ocultarTextoS6(){
+				var x;
+				x=$("#servicio6");
+				x.hide("blind",  300);
+			}
+			
+		
